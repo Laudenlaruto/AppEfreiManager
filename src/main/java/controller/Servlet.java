3e53da81d2bf7961fa9eebc9c6adf0513e128f94
laudenlaruto <laudenlaruto@hotmail.fr>
2017-12-14 13:19:20 +0100

    Updatestagiaire in servlet

--- src/main/java/controller/Servlet.java
+++ src/main/java/controller/Servlet.java
@@ -25,11 +25,12 @@ import sources.UserController;
  *
  * @author PC-Acta
  */
-
 public class Servlet extends HttpServlet {
+
     private static final String PAGE_PROFIL = "/WEB-INF/profil.jsp";
     private static final String PAGE_INDEX = "/WEB-INF/index.jsp";
     private static final String PAGE_DECONNEXION = "/WEB-INF/deconnexion.jsp";
+
     /**
      * Processes requests for both HTTP <code>GET</code> and <code>POST</code>
      * methods.
@@ -39,14 +40,12 @@ public class Servlet extends HttpServlet {
      * @throws ServletException if a servlet-specific error occurs
      * @throws IOException if an I/O error occurs
      */
-    
+
     protected void processRequest(HttpServletRequest request, HttpServletResponse response)
             throws ServletException, IOException {
         response.setContentType("text/html;charset=UTF-8");
-        
-        
-    }
 
+    }
 
     // <editor-fold defaultstate="collapsed" desc="HttpServlet methods. Click on the + sign on the left to edit the code.">
     /**
@@ -59,9 +58,8 @@ public class Servlet extends HttpServlet {
      */
     @Override
     protected void doGet(HttpServletRequest request, HttpServletResponse response)
-        throws ServletException, IOException {
-        
-        
+            throws ServletException, IOException {
+
         request.getServletContext().getRequestDispatcher(PAGE_INDEX).forward(request, response);
     }
 
@@ -74,45 +72,58 @@ public class Servlet extends HttpServlet {
      * @throws IOException if an I/O error occurs
      */
     @Override
-    protected void doPost(HttpServletRequest request, HttpServletResponse response)throws ServletException, IOException {
-        if(request.getParameter("actionlogin")!=null){
-            if(!request.getParameter("login").equals("")|| !request.getParameter("password").equals("")){
+    protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+        if (request.getParameter("actionlogin") != null) {
+            if (!request.getParameter("login").equals("") || !request.getParameter("password").equals("")) {
                 UserController usercontroller = new UserController();
-                User user = usercontroller.getUser(request.getParameter("login") , request.getParameter("password"));
-                
-                if(user!=null){
+                User user = usercontroller.getUser(request.getParameter("login"), request.getParameter("password"));
+
+                if (user != null) {
                     request.getSession().setAttribute("user", user);
                     StagiaireController stagiaireController = new StagiaireController();
                     List<Stagiaire> stagiaires = stagiaireController.getStagiaires(user.getId());
                     request.setAttribute("stagiaires", stagiaires);
                     request.setAttribute("size", stagiaires.size());
 
-                    request.getRequestDispatcher(PAGE_PROFIL).forward(request, response);                    
-               }else{
-                    request.getSession().setAttribute("status", "Erreur d'authentification / Utilisateur n'existe pas" );
-                    request.getServletContext().getRequestDispatcher(PAGE_INDEX).forward(request,response);
+                    request.getRequestDispatcher(PAGE_PROFIL).forward(request, response);
+                } else {
+                    request.getSession().setAttribute("status", "Erreur d'authentification / Utilisateur n'existe pas");
+                    request.getServletContext().getRequestDispatcher(PAGE_INDEX).forward(request, response);
                 }
-            }else{
-                   request.getSession().setAttribute("status", "Veuillez remplir les champs vides" );
-                 request.getServletContext().getRequestDispatcher(PAGE_INDEX).forward(request,response);
+            } else {
+                request.getSession().setAttribute("status", "Veuillez remplir les champs vides");
+                request.getServletContext().getRequestDispatcher(PAGE_INDEX).forward(request, response);
             }
-        }
-        
-        else if(request.getParameter("deconnexion")!=null){
+        } else if (request.getParameter("deconnexion") != null) {
             request.getSession().invalidate();
-            request.getServletContext().getRequestDispatcher(PAGE_DECONNEXION).forward(request,response);
-        }else if (request.getParameter("returnhome")!=null){
+            request.getServletContext().getRequestDispatcher(PAGE_DECONNEXION).forward(request, response);
+        } else if (request.getParameter("returnhome") != null) {
             request.getSession().invalidate();
-            request.getServletContext().getRequestDispatcher(PAGE_INDEX).forward(request,response);
+            request.getServletContext().getRequestDispatcher(PAGE_INDEX).forward(request, response);
 
-        }
-        else if (request.getParameter("getuser")!=null){
-           StagiaireController stagiaireController = new StagiaireController();
-           Stagiaire stagiaire = stagiaireController.getStagiaire(request.getParameter("getuser"));
+        } else if (request.getParameter("getuser") != null) {
+            StagiaireController stagiaireController = new StagiaireController();
+            Stagiaire stagiaire = stagiaireController.getStagiaire(request.getParameter("getuser"));
             String json = new Gson().toJson(stagiaire);
             response.setContentType("application/json");
             response.setCharacterEncoding("UTF-8");
             response.getWriter().write(json);
+
+        } else if (request.getParameter("actionupdatestagiaire") != null) {
+  
+            String[] id = request.getParameterValues("id");
+            for (int i=0; i<id.length;i++){
+                Stagiaire stagiaire = new Stagiaire();
+                stagiaire.setCdc(Boolean.valueOf(request.getParameter(id[i]+".cdc")));
+                stagiaire.setCdc(Boolean.valueOf(request.getParameter(id[i]+".fiche_visite")));
+                stagiaire.setCdc(Boolean.valueOf(request.getParameter(id[i]+".fiche_evaluation")));
+               
+                
+            }
+            
+            
+
+          
 
         }
     }
@@ -126,6 +137,5 @@ public class Servlet extends HttpServlet {
     public String getServletInfo() {
         return "Short description";
     }// </editor-fold>
-    
-    
+
 }
